64d576f91c5489cfa0a00bed72ec446844c93231
ztelur <ztelur@gmail.com>
2018-10-08 22:04:55 +0800

    update

---
+++ chapter12-cloud-security/chapter12-authorization-server/src/test/java/com/xuan/chapter12/authorization/server/Chapter12AuthorizationServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xuan.chapter12.authorization.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Chapter12AuthorizationServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
